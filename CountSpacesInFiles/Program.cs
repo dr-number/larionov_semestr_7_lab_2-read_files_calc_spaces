35481f9ba9c77cdbb33314a9bdab21b8910d5e7f
Lar97 <dr.number@yandex.ru>
2025-01-19 00:59:50 +0700

    correct info

--- CountSpacesInFiles/Program.cs
+++ CountSpacesInFiles/Program.cs
@@ -11,39 +11,62 @@ namespace CountSpacesInFiles
     {
         private const String INFO_ABOUT_VARIANT_A = "Cчитали файл полностью асинхронно и потом посчитали пробелы.";
         private const String INFO_ABOUT_VARIANT_B = "Cчитали файл построчно и для каждой строчки считаем пробелы.";
+
+        public static void ShowMessage(string message)
+        {
+            Console.ResetColor();
+            Console.WriteLine(message);
+        }
+        public static void ShowMessageError(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(message);
+        }
+        public static void ShowMessageSuccess(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(message);
+        }
+
         static async Task Main(string[] args)
         {
             string directoryPath = @"D:\semestr_7_technology_programming_texts";
 
-            if (Directory.Exists(directoryPath))
+            if (!Directory.Exists(directoryPath))
             {
-                var txtFiles = Directory.GetFiles(directoryPath, "*.txt");
-
-                foreach (var file in txtFiles)
-                {
-                    Console.WriteLine($"Обработка файла: {file}...");
-
-                    // Способ a: Считываем файл полностью асинхронно
-                    var startTime = DateTime.Now;
-                    int totalSpacesAsync = await CountSpacesInFileAsync(file);
-                    var endTime = DateTime.Now;
-                    var elapsedTimeAsync = endTime - startTime;
-                    Console.WriteLine($"\nСпособ a.\n{INFO_ABOUT_VARIANT_A}\nФайл полностью асинхронно прочитан. Время: {elapsedTimeAsync.TotalMilliseconds} ms, Количество пробелов: {totalSpacesAsync}");
-
-                    // Способ b: Считываем файл построчно асинхронно
-                    startTime = DateTime.Now;
-                    int totalSpacesLineByLine = await CountSpacesLineByLineAsync(file);
-                    endTime = DateTime.Now;
-                    var elapsedTimeLineByLine = endTime - startTime;
-                    Console.WriteLine($"\nСпособ б.\n{INFO_ABOUT_VARIANT_B}\nФайл полностью асинхронно прочитан. Время: {elapsedTimeLineByLine.TotalMilliseconds} ms, Количество пробелов: {totalSpacesLineByLine}");
-
-                    Console.WriteLine();
-                }
+                ShowMessageError("Папка не найдена!\n");
+                return;
             }
-            else
+            
+            var txtFiles = Directory.GetFiles(directoryPath, "*.txt");
+            if (txtFiles == null || txtFiles.Length == 0)
             {
-                Console.WriteLine("Папка не найдена");
+                ShowMessageError($"В папке \"{directoryPath}\" нет txt файлов!\n");
+                return;
             }
+
+            foreach (var file in txtFiles)
+            {
+                long fileSize = new FileInfo(file).Length;
+                ShowMessageSuccess($"Обработка файла: {file} размер файла {fileSize} байт...");
+
+                // Способ a: Считываем файл полностью асинхронно
+                var startTime = DateTime.Now;
+                int totalSpacesAsync = await CountSpacesInFileAsync(file);
+                var endTime = DateTime.Now;
+                var elapsedTimeAsync = endTime - startTime;
+                ShowMessage($"\nСпособ a.\n{INFO_ABOUT_VARIANT_A}\nФайл полностью асинхронно прочитан. Время: {elapsedTimeAsync.TotalMilliseconds} ms, Количество пробелов: {totalSpacesAsync}");
+
+                // Способ b: Считываем файл построчно асинхронно
+                startTime = DateTime.Now;
+                int totalSpacesLineByLine = await CountSpacesLineByLineAsync(file);
+                endTime = DateTime.Now;
+                var elapsedTimeLineByLine = endTime - startTime;
+                ShowMessage($"\nСпособ б.\n{INFO_ABOUT_VARIANT_B}\nФайл полностью асинхронно прочитан. Время: {elapsedTimeLineByLine.TotalMilliseconds} ms, Количество пробелов: {totalSpacesLineByLine}");
+
+                Console.WriteLine();
+            }
+      
         }
 
         // Способ a: Считываем файл полностью и считаем пробелы
